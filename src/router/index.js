8dc7394bd74892d9f42330e7368daa750be3c0f0
Matheus Andrade <matheus.andrade@cepedi.org.br>
2021-11-08 03:56:17 -0300

    feat: create medical record view 07

--- src/router/index.js
+++ src/router/index.js
@@ -99,6 +99,11 @@ const routes = [
     path: '/nutritionist/evaluation/medicalRecords04',
     name: 'medicalRecords04',
     component: () => import('../views/NutritionistEvaluation/MedicalRecords04.vue')
+  },
+  {
+    path: '/nutritionist/evaluation/medicalRecords07',
+    name: 'medicalRecords07',
+    component: () => import('../views/NutritionistEvaluation/MedicalRecords07.vue')
   }
 ]
 
