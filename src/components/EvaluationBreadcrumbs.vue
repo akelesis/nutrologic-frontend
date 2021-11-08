18000fa3e8e2357a046223bb1332054c6e6900a4
Marcus Tavares <marcus.tavares@outlook.com>
2021-11-07 22:15:58 -0300

    feat: First screen nutritionist evaluation

--- src/components/EvaluationBreadcrumbs.vue
+++ src/components/EvaluationBreadcrumbs.vue
@@ -1,25 +1,16 @@
 <template>
   <div :style="cssVars" class="EvaluationBreadcrumbs">
     <div class="dot" :class="step > 0 ? 'active' : ''"></div>
-<<<<<<< Updated upstream
-    <div class="dash" :class="step > 1 ? 'active' : ''"></div>
-    <div class="dot" :class="step > 1  ? 'active' : ''"></div>
-    <div class="dash" :class="step > 2  ? 'active' : ''"></div>
-    <div class="dot" :class="step > 2  ? 'active' : ''"></div>
-    <div class="dash" :class="step > 3  ? 'active' : ''"></div>
-    <div class="dot" :class="step > 3  ? 'active' : ''"></div>
-=======
-    <span v-for="n in dashCounter" :key="n" class="EvaluationBreadcrumbs">
-      <div class="dash" :class="step > n ? 'active' : ''"></div>
-      <div class="dot" :class="step > n ? 'active' : ''"></div>
+    <span v-for="n in stepsCounter-1" :key="n" class="EvaluationBreadcrumbs">
+      <div class="dash" :class="step > n+1 ? 'active' : ''"></div>
+      <div class="dot" :class="step > n+1 ? 'active' : ''"></div>
     </span>
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
 export default {
-  props: ['step', 'evalType'],
+  props: ['step', 'evalType', 'stepsCounter'],
   computed: {
     cssVars () {
       return {
--- src/views/NutritionistEvaluation/MedicalRecords01.vue
+++ src/views/NutritionistEvaluation/MedicalRecords01.vue
@@ -11,20 +11,41 @@
             <span>Júlia Santana</span>
             <i class="far fa-file-alt" />
           </div>
-          <evaluation-breadcrumbs step="2" stepsCounter="10" />
+          <evaluation-breadcrumbs step="1" :stepsCounter="10" />
           <p id="medical-records-title">Cadastro de Prontuário e Avaliação</p>
           <div class="medical-records-content-container">
-            <default-text-area placeholder="Exames Laboratoriais"/>
-            <default-text-area placeholder="Medicamentos em uso"/>
-            <div class="medical-record-alergy-container">
-              <p class="question-title">Alergia ou intolerância alimentar:</p>
-              <div class="radius-group">
-                <nutritionist-radius id="positive-alergy" name="alergy-radius" value="Sim" />
-                <nutritionist-radius id="positive-alergy" name="alergy-radius" value="Não" />
+            <div class="nutritionist-input-group">
+              <default-input placeholder="Número do prontuário"/>
+            </div>
+            <div class="nutritionist-input-group">
+              <default-input placeholder="Acompanhante"/>
+              <default-input placeholder="Diagnóstico"/>
+            </div>
+            <div class="desease-info-container">
+              <p class="question-title">Estadiamento:</p>
+              <div class="desease-stage-container">
+                <div class="radius-group">
+                  <nutritionist-radius id="one" name="desease-stage-radius" value="I" />
+                  <nutritionist-radius id="two" name="desease-stage-radius" value="II" />
+                  <nutritionist-radius id="three" name="desease-stage-radius" value="III" />
+                  <nutritionist-radius id="four" name="desease-stage-radius" value="IV" />
+                </div>
+                <div class="tmng-fields">
+                  <label for="t-field">T</label>
+                  <input type="text"  class="tmng-field" id="t-field">
+                  <label for="m-field">M</label>
+                  <input type="text" id="m-field" class="tmng-field">
+                  <label for="n-field">N</label>
+                  <input type="text"  id="n-field" class="tmng-field">
+                  <label for="g-field">G</label>
+                  <input type="text"  id="g-field" class="tmng-field">
+                </div>
               </div>
             </div>
+            <default-text-area placeholder="Tratamento" :rows="2"/>
           </div>
         </div>
+        <green-button label="Próximo"/>
       </div>
     </main>
     <main-footer />
@@ -35,15 +56,19 @@
 import Header from '../../components/Header.vue'
 import MainFooter from '../../components/MainFooter.vue'
 import EvaluationBreadcrumbs from '../../components/EvaluationBreadcrumbs.vue'
-import DefaultTextArea from '../../components/DefaultTextArea.vue'
 import NutritionistRadius from '../../components/NutritionistRadius.vue'
+import DefaultInput from '../../components/DefaultInput.vue'
+import DefaultTextArea from '../../components/DefaultTextArea.vue'
+import GreenButton from '../../components/GreenButton.vue'
 export default {
   components: {
     Header,
     MainFooter,
     EvaluationBreadcrumbs,
+    NutritionistRadius,
+    DefaultInput,
     DefaultTextArea,
-    NutritionistRadius
+    GreenButton
   },
   data () {
     return {
@@ -93,6 +118,8 @@ export default {
   flex-direction: column;
   min-width: 60vw;
   height: 100%;
+  align-items: center;
+  margin: 20px 0;
 }
 
 .patient-name-file-container {
@@ -128,19 +155,50 @@ export default {
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
+  max-width: 45vw;
+  min-width: 30vw;
+}
+
+.nutritionist-input-group {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 30px 0;
 }
 
 .radius-group {
   display: flex;
 }
 
-.medical-record-alergy-container {
-  width: 50vw;
+.desease-stage-container {
+  justify-content: flex-start;
+  margin: 0 0 40px 0;
+}
+
+.desease-info-container {
+  width: 100%;
 }
 
 .question-title {
   font-size: 20px;
   text-align: left;
+}
+
+.tmng-field {
+  width: 40px;
+  border: none;
+  border-bottom: 1px solid #C4C4C4;
+  transition: .2s;
+}
+
+.tmng-field:focus {
+  outline: none;
+  border-bottom: 3px solid #58e28f;
+}
+
+.desease-stage-container {
+  display: flex;
+  justify-content: space-between;
 }
 
 </style>
